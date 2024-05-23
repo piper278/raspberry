cb31f5f6603cc99b42cc18ea44b0d73aa116fcb0
piper278 <143884711+piper278@users.noreply.github.com>
2024-05-23 20:40:14 +0800

    gpio_led.py

--- led.py
+++ led.py
@@ -1,16 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-
 LED_PIN = 18
-
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
 GPIO.output(LED_PIN, GPIO.HIGH)
-
 time.sleep(3)
-
 GPIO.output(LED_PIN, GPIO.LOW)
 
 GPIO.cleanup()
